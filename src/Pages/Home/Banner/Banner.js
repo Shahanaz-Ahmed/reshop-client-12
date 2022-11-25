8e58210ef1b4f7da87d6adb10896531874fee64a
ShahanazAhmedNishi <asahanaz207@gmail.com>
2022-11-25 12:55:56 +0600

    first

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import banner from "../../../assets/images/Banner.jpg";
+const Banner = () => {
+  return (
+    <div>
+      <h2>This Banner</h2>
+      <img src={banner} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
